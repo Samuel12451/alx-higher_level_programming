58070d4a13f6e0f0f9699d009b5076efcbfbc003
samuel12451 <samuel.tesema.st@gmail.com>
2022-11-29 10:55:58 -0800

    task done

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    copy = ''
-    if len(str) == 0:
-        return str
+    s = ""
     for i in range(len(str)):
         if i != n:
-            copy += str[i]
-    return copy
-if __name__ == '__main__':
-    print(remove_char_at("Holberton School", 3))
+            s = s + str[i]
+    return (s)
