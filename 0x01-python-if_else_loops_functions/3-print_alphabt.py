5112aade2ab932fe9267deea65f5c53eb7d9a763
samuel12451 <samuel.tesema.st@gmail.com>
2022-11-29 10:43:24 -0800

    task completed

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for c in 'abcdefghijklmnopqrstuvwxyz':
+    if 'e' in c:
+            continue
+    elif 'q' in c:
+            continue
+    else:
+            print("{}".format(c), end='')
